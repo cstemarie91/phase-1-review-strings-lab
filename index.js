5f37877f3b34e23841ff598274addb8116327f30
+ <+>
2024-06-17 21:14:58 -0400

    Lab Final

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+const upperCaseCurrentUser = currentUser.toUpperCase();
+const firstInitial = currentUser[0];
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${upperCaseCurrentUser}!`
+const shortGreeting = `Welcome, ${firstInitial}!`
